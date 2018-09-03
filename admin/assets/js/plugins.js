43b7f8cfe6cc9c7a0fcb182d60bc3efd2a1fe138
Dwyrain <dwyrain@rambler.ru>
2018-09-03 11:16:48 +0400

    UM-3 добавлены иконки (js edition)

---
+++ admin/assets/js/plugins.js
@@ -0,0 +1,31 @@
+'use strict';
+jQuery(function($){
+    window.um_add_plugin_icons = function(info){
+        $('#the-list tr[data-plugin]').each(function(k,v){
+            let plugin_path = $(v).data('plugin');
+            let slug_parts = plugin_path.split('/');
+            let slug = slug_parts[0];
+
+            let update_class = '';
+
+            let is_auto_update = false;
+            let is_update_disabled = false;
+            if(!info['filters']['disable_auto_updates'][slug] && info['auto_update_allowed']){
+                is_auto_update = true;
+            }
+            if(info['updates_disabled'] || info['filters']['disable_updates'][slug]){
+                is_update_disabled = true;
+            }
+
+            if(is_auto_update){
+                update_class = 'purple';
+            }
+            if(is_update_disabled){
+                update_class = 'red';
+            }
+
+            $(v).find('.check-column').append('<span class="dashicons dashicons-update um-plugin-status ' + update_class + '"></span>');
+
+        });
+    }
+});
