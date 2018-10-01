5d9c32e7f65ed95603e21f8ecefa52deab2373f8
alex.kovalevv <alex.kovalevv@gmail.com>
2018-10-01 16:04:28 +0300

    Добавлена проверка на совместимость

--- webcraftic-updates-manager.php
+++ webcraftic-updates-manager.php
@@ -10,74 +10,87 @@
 	 * Author URI: https://clearfy.pro
 	 */
 
-	if( defined('WUP_PLUGIN_ACTIVE') || (defined('WCL_PLUGIN_ACTIVE') && !defined('LOADING_UPDATES_MANAGER_AS_ADDON')) ) {
-		function wbcr_upm_admin_notice_error()
-		{
-			?>
-			<div class="notice notice-error">
-				<p><?php _e('We found that you have the "Clearfy - disable unused features" plugin installed, this plugin already has update manager functions, so you can deactivate plugin "Update manager"!'); ?></p>
-			</div>
-		<?php
-		}
+	define('WUP_PLUGIN_VERSION', '1.0.7');
 
-		add_action('admin_notices', 'wbcr_upm_admin_notice_error');
+	define('WUP_PLUGIN_DIR', dirname(__FILE__));
+	define('WUP_PLUGIN_BASE', plugin_basename(__FILE__));
+	define('WUP_PLUGIN_URL', plugins_url(null, __FILE__));
 
+	#comp remove
+	// the following constants are used to debug features of diffrent builds
+	// on developer machines before compiling the plugin
+
+	// build: free, premium, ultimate
+	if( !defined('BUILD_TYPE') ) {
+		define('BUILD_TYPE', 'free');
+	}
+	// language: en_US, ru_RU
+	if( !defined('LANG_TYPE') ) {
+		define('LANG_TYPE', 'en_EN');
+	}
+	// license: free, paid
+	if( !defined('LICENSE_TYPE') ) {
+		define('LICENSE_TYPE', 'free');
+	}
+
+	// wordpress language
+	if( !defined('WPLANG') ) {
+		define('WPLANG', LANG_TYPE);
+	}
+	// the compiler library provides a set of functions like onp_build and onp_license
+	// to check how the plugin work for diffrent builds on developer machines
+
+	if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
+		require('libs/onepress/compiler/boot.php');
+		// creating a plugin via the factory
+	}
+	// #fix compiller bug new Factory000_Plugin
+	#endcomp
+
+	if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
+		require_once(WUP_PLUGIN_DIR . '/libs/factory/core/includes/check-compatibility.php');
+		require_once(WUP_PLUGIN_DIR . '/libs/factory/clearfy/includes/check-clearfy-compatibility.php');
+	}
+
+	$plugin_info = array(
+		'prefix' => 'wbcr_updates_manager_',//wbcr_upm_
+		'plugin_name' => 'wbcr_updates_manager',
+		'plugin_title' => __('Webcraftic Updates Manager', 'webcraftic-updates-manager'),
+		'plugin_version' => WUP_PLUGIN_VERSION,
+		'plugin_build' => BUILD_TYPE,
+		//'updates' => WUP_PLUGIN_DIR . '/updates/'
+	);
+
+	/**
+	 * Проверяет совместимость с Wordpress, php и другими плагинами.
+	 */
+	$compatibility = new Wbcr_FactoryClearfy000_Compatibility(array_merge($plugin_info, array(
+		'plugin_already_activate' => defined('WUP_PLUGIN_ACTIVE'),
+		'plugin_as_component' => defined('LOADING_UPDATES_MANAGER_AS_ADDON'),
+		'plugin_dir' => WUP_PLUGIN_DIR,
+		'plugin_base' => WUP_PLUGIN_BASE,
+		'plugin_url' => WUP_PLUGIN_URL,
+		'required_php_version' => '5.3',
+		'required_wp_version' => '4.2.0',
+		'required_clearfy_check_component' => true
+	)));
+
+	/**
+	 * Если плагин совместим, то он продолжит свою работу, иначе будет остановлен,
+	 * а пользователь получит предупреждение.
+	 */
+	if( !$compatibility->check() ) {
 		return;
-	} else {
+	}
 
-		define('WUP_PLUGIN_ACTIVE', true);
+	define('WUP_PLUGIN_ACTIVE', true);
 
-		define('WUP_PLUGIN_DIR', dirname(__FILE__));
-		define('WUP_PLUGIN_BASE', plugin_basename(__FILE__));
-		define('WUP_PLUGIN_URL', plugins_url(null, __FILE__));
+	if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
+		require_once(WUP_PLUGIN_DIR . '/libs/factory/core/boot.php');
+	}
 
-		#comp remove
-		// the following constants are used to debug features of diffrent builds
-		// on developer machines before compiling the plugin
+	require_once(WUP_PLUGIN_DIR . '/includes/class.plugin.php');
 
-		// build: free, premium, ultimate
-		if( !defined('BUILD_TYPE') ) {
-			define('BUILD_TYPE', 'free');
-		}
-		// language: en_US, ru_RU
-		if( !defined('LANG_TYPE') ) {
-			define('LANG_TYPE', 'en_EN');
-		}
-		// license: free, paid
-		if( !defined('LICENSE_TYPE') ) {
-			define('LICENSE_TYPE', 'free');
-		}
-
-		// wordpress language
-		if( !defined('WPLANG') ) {
-			define('WPLANG', LANG_TYPE);
-		}
-		// the compiler library provides a set of functions like onp_build and onp_license
-		// to check how the plugin work for diffrent builds on developer machines
-
-		if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
-			require('libs/onepress/compiler/boot.php');
-			// creating a plugin via the factory
-		}
-		// #fix compiller bug new Factory000_Plugin
-		#endcomp
-
-		if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
-			require_once(WUP_PLUGIN_DIR . '/libs/factory/core/boot.php');
-		}
-
-		require_once(WUP_PLUGIN_DIR . '/includes/class.plugin.php');
-
-		if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
-			new WUP_Plugin(__FILE__, array(
-				'prefix' => 'wbcr_updates_manager_',//wbcr_upm_
-				'plugin_name' => 'wbcr_updates_manager',
-				'plugin_title' => __('Webcraftic Updates Manager', 'webcraftic-updates-manager'),
-				'plugin_version' => '1.0.7',
-				'required_php_version' => '5.2',
-				'required_wp_version' => '4.2',
-				'plugin_build' => BUILD_TYPE,
-				//'updates' => WUP_PLUGIN_DIR . '/updates/'
-			));
-		}
+	if( !defined('LOADING_UPDATES_MANAGER_AS_ADDON') ) {
+		new WUP_Plugin(__FILE__, $plugin_info);
 	}
